fc88ac99ddc09647f79c60859dba0e3c50d7d773
rahul.raviprasad <rahul.raviprasad@gmail.com>
2017-04-14 23:50:08 +0530

    added an examplefile to show linear regression using sklearn

---
+++ LinearRegression/linearRegressionExample.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Every algorithm is exposed in scikit-learn via an ''Estimator'' object.
+# For instance a linear regression is implemented as an Estimator object.
+# for more read: http://scikit-learn.org/stable/tutorial/statistical_inference/settings.html
+from sklearn.linear_model import LinearRegression
