6f6035c83a7a123e5db2586f48a5890f0885f01c
rahul.raviprasad <rahul.raviprasad@gmail.com>
2017-04-16 00:53:43 +0530

    svm on generated data

--- supportVectorMachines/svmExample.py
+++ supportVectorMachines/svmExample.py
@@ -18,4 +18,29 @@ for m, b in [(1, 0.65), (0.5, 1.6), (-0.2, 2.9)]:
 
 plt.xlim(-1, 3.5);
 
+
+from sklearn.svm import SVC # "Support Vector Classifier"
+clf = SVC(kernel='linear')
+clf.fit(X, y)
+
+def plot_svc_decision_function(clf, ax=None):
+    """Plot the decision function for a 2D SVC"""
+    if ax is None:
+        ax = plt.gca()
+    x = np.linspace(plt.xlim()[0], plt.xlim()[1], 30)
+    y = np.linspace(plt.ylim()[0], plt.ylim()[1], 30)
+    Y, X = np.meshgrid(y, x)
+    P = np.zeros_like(X)
+    for i, xi in enumerate(x):
+        for j, yj in enumerate(y):
+            P[i, j] = clf.decision_function([xi, yj])
+    # plot the margins
+    ax.contour(X, Y, P, colors='k',
+               levels=[-1, 0, 1], alpha=0.5,
+               linestyles=['--', '-', '--'])
+
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='spring')
+plot_svc_decision_function(clf);
+
+
 plt.show()
